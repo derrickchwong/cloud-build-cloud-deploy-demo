041c2d5adc9ff6400f94bc6bf445ad295d123de6
Derrick Wong <derrick.chwong@gmail.com>
2022-09-08 15:47:45 +0800

    add greeting msg

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -13,9 +14,9 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public String hi(){
-    return "Hello World 7";
+	@GetMapping("/hi/{name}")
+	public String hi(@PathVariable String name){
+    return "Hello " + name.toUpperCase() + "! How are you?";
   }
 
 }
